011be9bcd8203afe6aec359546ce00d7b01189a2
Mishanana20 <mishanana20@gmail.com>
2022-11-01 01:26:42 +0700

    Добавил вывод списка тестов

---
+++ Frames/AllTests_Frame.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Login_App.model;
+
+namespace Login_App.Frames
+{
+    /// <summary>
+    /// Логика взаимодействия для AllTests_Frame.xaml
+    /// </summary>
+    public partial class AllTests_Frame : Page
+    {
+        public List<TestName> testNames = new List<TestName>();
+
+        public AllTests_Frame()
+        {
+          
+            
+            InitializeComponent();
+            Loaded += TestAddElements_Loaded;            
+        }
+
+
+        public void TestAddElements_Loaded(object sender, RoutedEventArgs e)
+        {
+            TestName bio = new TestName() { name = "First Test", text = "Description for the Ferst Text"};
+            bio.name = "First Test"; bio.text = "Description for the Ferst Text";
+            testNames.Add(bio);
+            testNames.Add(new TestName { name = "Биология", text = "Первый тест по биологии" });
+            testNames.Add(new TestName { name = "География", text = "Контрольная до 25-11-22" });
+            testNames.Add(new TestName { name = "Математика", text = "Тест для повторения основ" });
+            testNames.Add(new TestName { name = "Биология", text = "Первый тест по биологии" });
+            testNames.Add(new TestName { name = "География", text = "Контрольная до 25-11-22" });
+            testNames.Add(new TestName { name = "Математика", text = "Тест для повторения основ" });
+            testNames.Add(new TestName { name = "Биология", text = "Первый тест по биологии" });
+            testNames.Add(new TestName { name = "География", text = "Контрольная до 25-11-22" });
+            testNames.Add(new TestName { name = "Математика", text = "Тест для повторения основ" });
+            lbName.ItemsSource = testNames;
+            //lbName.ItemsSource = new object[] { bio };
+        }
+    }
+}
--- MainMenu.xaml.cs
+++ MainMenu.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -19,10 +21,29 @@ namespace Login_App
     /// </summary>
     public partial class MainMenu : Window
     {
-        public MainMenu()
+        public MainMenu()       
         {
-            InitializeComponent();
+            InitializeComponent();      
+            CurFrame.Navigate(new Frames.StartFrame());
         }
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        private const int GWL_STYLE = -16;
+        private const int WS_MAXIMIZEBOX = 0x10000;
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            var hwnd = new WindowInteropHelper((Window)sender).Handle;
+            var value = GetWindowLong(hwnd, GWL_STYLE);
+            SetWindowLong(hwnd, GWL_STYLE, (int)(value & ~WS_MAXIMIZEBOX));
+        }
+
+
+
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -30,6 +51,16 @@ namespace Login_App
             {
                 this.DragMove();
             }
+        }
+
+        private void ButtonFrameAllTests_Click(object sender, RoutedEventArgs e)
+        {
+            CurFrame.Navigate(new Frames.AllTests_Frame());
+        }
+
+        private void ButtonOpenTest_Click(object sender, RoutedEventArgs e)
+        {
+            CurFrame.Navigate(new Frames.Test());
         }
     }
 }
